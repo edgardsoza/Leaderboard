292db9811f5c13e2d241b63a422013e0a3c42c6e
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-15 16:12:31 -0600

    fixing linter

--- src/modules/display-score.js
+++ src/modules/display-score.js
@@ -1,14 +1,14 @@
 const table = document.querySelector('.table-scores');
 
-const displayscores = async () => {
-  const result = await fetch (
+const displayscores = async() => {
+  const result = await fetch(
     'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7YMDhAdb7FXRHals0VRW/scores/',
   );
 
   const scoresdata = await result.json();
   let scoresarray = scoresdata.result;
-  scoresarray = scoresarray.sort((a,b) => b.score - a.score);
-  table.innerHTML='';
+  scoresarray = scoresarray.sort((a, b) => b.score - a.score);
+  table.innerHTML = '';
   scoresarray.forEach((score) => {
     const tableitem = document.createElement('tr');
     tableitem.innerHTML = `${score.user} : ${score.score}`;
