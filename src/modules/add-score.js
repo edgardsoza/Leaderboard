39b2db7a23e0aab7d9fcaab5516dcd37d1081d52
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-15 15:57:00 -0600

    add submit function and display function

---
+++ src/modules/add-score.js
@@ -0,0 +1,16 @@
+const name = document.getElementById('name');
+const scores = document.getElementById('score');
+
+ const addscore = async() => {
+   if (name.value !== '' & scores.value !== '') {
+   await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7YMDhAdb7FXRHals0VRW/scores/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({user: name.value, score: scores.value}),
+    })};
+};
+
+export {addscore};
